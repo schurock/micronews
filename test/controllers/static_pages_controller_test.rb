3c0a1e8e29ad32f779a8143d0002f4498abf8c95
Tim Schurig <t.schurig@gmail.com>
2015-08-15 12:26:01 +0000

    add solved exes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title ="micronews"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "home | micronews"
+    assert_select "title", "home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "help | micronews"
+    assert_select "title", "help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "about | micronews"
+    assert_select "title", "about | #{@base_title}"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "contact | #{@base_title}"
+  end
+  
 end
