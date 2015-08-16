b4e02e6f4c6398e96ddc5cb8c311a8a370b3b993
Tim Schurig <t.schurig@gmail.com>
2015-08-16 19:59:16 +0000

    fix typo

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,6 +12,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
      
      get signup_path
      assert_template 'users/new'
-     assert_select "title", full_title("Sign up")
+     assert_select "title", full_title("sign up")
    end
 end
